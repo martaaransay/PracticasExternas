f04dbd99903f9481f97e03b1e26ce267b03813cf
martaaransay <martaaransay@gmail.com>
2026-09-09 13:25:50 +0200

    inicio de descarga de genomas (falta subprocess) - definidas las flags

---
+++ scripts/T1_abundancia_Integrall.py
@@ -0,0 +1,91 @@
+# Import librerías 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Ruta a la tabla de Integrall
+path = "/home/marta/CNB/PracticasExternas/data/Integrall/Integrons-Analysis-TJ-V23/Integrons-Analysis-TJ-V23_v2.xlsx"
+encabezado = 3 # Número de líneas en el encabezado a skippear
+
+df = pd.read_excel(path, skiprows = encabezado) # Lectura del excel
+df.columns = df.columns.str.strip() # Limpieza de columnas
+
+def abundancia_pc(df, columnas, path_out = "./results/pruebas_inicial_integrall/abundancia_pc_especie.xlsx"):
+    """
+    Función para generar un .xlsx con el conteo de promotores sobreuna tabla...
+    """
+    tabla_conteo = pd.crosstab(df[columnas[0]], df[columnas[1]]) # Conteo del número de Pc - especie
+    # Columna con el conteo total de Pc por especie
+    tabla_conteo["Total"] = tabla_conteo.sum(axis=1) 
+    # Ordenación de la tabla de mayor a menor número de Pc
+    tabla_conteo = tabla_conteo.sort_values("Total", ascending=False) # Ordena del que más al que menos
+
+    # ------------Filtro especies ESKAPEE------------
+    eskapee = ["Escherichia coli", 
+                "Klebsiella pneumoniae", 
+                "Pseudomonas aeruginosa",
+                "Acinetobacter baumannii",
+                "Enterobacter spp."] 
+    # Condición especie ESKAPEE 
+    especies = tabla_conteo.index # Nombre de las filas
+    # Condición: Coincida con ESKAPEE o empiece por Enterobacter (.str para aplicar startswith)
+    cond_eskapee = especies.isin(eskapee) | especies.str.startswith("Enterobacter ") 
+
+    # Nueva tabla con el conteo único para el filtro
+    tabla_conteo_ESKAPEE = tabla_conteo[cond_eskapee].copy()
+    # Nueva fila con el sumatorio de todas las Enterobacter
+    cond_enterobacter = tabla_conteo_ESKAPEE.index.str.startswith("Enterobacter ")
+    suma_enterobacter = tabla_conteo_ESKAPEE[cond_enterobacter].sum()
+    # Adición de la nueva fila generada
+    tabla_conteo_ESKAPEE.loc["Enterobacter spp."] = suma_enterobacter
+ 
+    # ----------Nuevo xlsx con ambos conteos-----------
+    with pd.ExcelWriter(path_out) as w:
+        tabla_conteo.to_excel(w, sheet_name="Conteo")
+        tabla_conteo_ESKAPEE.to_excel(w, sheet_name="Conteo_ESKAPEE")
+
+    # -----------Gráficas de ambos conteos------------
+    # Colores para los Pc:
+    colores = ["#A7A8A7", "#B3924F", "#984FB3", "#AAAA00", "#FF5733", 
+               "#FF8D1A", "#FFE400", "#FF96E8", "#96FFE5", "#4F80B3" ]
+    
+    plt.figure(figsize=(10, 6))
+
+    # Gráfica 1: Abundancia general (sin tener en cuenta la especie)
+    plt.subplot(1, 2, 1)
+    # Eliminación columna Total (no es Pc a printear)
+    df_plot = tabla_conteo.drop(columns=["Total"]).T # Transposición de la tabla
+    # Nuevo df con la abundancia general de los promotores (sin separar por especie individual)
+    df_total = df_plot.sum(axis=1) 
+
+    # Generación del gráfico de barras con los colores definidos anteriormente 
+    df_total.plot(kind="bar", color=colores, edgecolor="black")
+
+    # Etiquetas del gráfico
+    plt.title("Abundancia relativa de promotores", fontweight="bold")
+    plt.xlabel("Promotores", fontweight="bold")
+    plt.ylabel("Conteo", fontweight="bold")
+    plt.xticks(rotation=45, ha="right")
+
+    # Gráfica 2: Especies ESKAPEE
+    plt.subplot(1, 2, 2)
+    
+    # Filtro de las especies ESKAPEE y eliminación columna Total 
+    df_plot_eskapee = tabla_conteo_ESKAPEE.loc[eskapee].drop(columns=["Total"]) 
+
+    df_plot_eskapee.plot(kind="bar", 
+                         ax=plt.gca(), # Para que no genere dos gráficos
+                         stacked=True, # Barras apiladas
+                         color=colores, 
+                         edgecolor="black")
+
+    plt.title("Distribución de Promotores en especies ESKAPEE", fontweight="bold")
+    plt.xlabel("Especies ESKAPEE", fontweight="bold")
+    plt.ylabel("Conteo", fontweight="bold")
+    plt.xticks(rotation=45, ha="right")
+    # Leyenda de los gráficos (colores de los Pc)
+    plt.legend(title="Promotor (Pc)", bbox_to_anchor=(1.05, 1), loc="upper left")
+    plt.tight_layout() # AJuste de tamaño
+    plt.show()
+    return
+
+abundancia_pc(df, ["Organism", "Pc"])
---
+++ scripts/T2_descarga_genomas.py
@@ -0,0 +1,54 @@
+import subprocess
+# subprocess permite ejecutar comandos en la terminal (ej. datasets)
+
+###FUNCIÓN PARA DEFINIR EL FILTRADO DEL GENOMA
+
+def definir_flags(
+          assembly_source = None, # RefSeq | GenBank | None =todas
+          assembly_level = None, # comma-separated: chromosome | complete | contig | scaffold | None =todas
+          annotated = None, # Si no es None, excluye genomas anotados
+          exclude_atypical = None, # Si no es None, excluye assemblies atípicos
+          exclude_multi_isolate = None, # Si no es None, excluye assemblies de proyectos multi-isolate
+          mag = None, # Si es True, solo usa mags; si es false, los excluye
+          ):
+    
+    # Lista de las flags
+
+    flags = []
+
+    if assembly_source:
+        flags += ["--assembly-source", assembly_source]
+    
+    if assembly_level:
+        # Comma-separated:
+        flags += ["--assembly-level", ",".join(assembly_level)]
+
+    if annotated:
+        flags.append("--annotated")
+
+    if exclude_atypical:
+        flags.append("--exclude-atypical")
+
+    if exclude_multi_isolate:
+        flags.append("--exclude-multi-isolate")
+
+    if mag != None:
+        if mag:
+            flags += ["--mag", "only"]
+        elif not mag:
+            flags += ["--mag", "exclude"]
+
+    return flags
+
+
+###FUNCIÓN PARA DEFINIR EL ORIGEN DE LOS DATOS
+def origen_datos(origen, # taxon | accession
+                 nombres #Lista de los valores
+                 ):
+    if origen not in ["taxon", "accession"]:
+        raise ValueError(f"No se ha introducido una de las posibilidades: taxon | accession ({origen})")
+    
+    ## * permite desempaquetar, para no generar una lista anidada
+    return [origen, *nombres]
+
+def descargar_genomas()
